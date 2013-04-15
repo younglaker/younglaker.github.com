ff81bbff4284677fcfb93cca217d09554b6c1288
laker <younglaker8@gmail.com>
2013-04-15 13:13:21 +0800

    merge js into laker.js

--- js/mainjs.js
+++ js/mainjs.js
@@ -5,50 +5,18 @@
 		$(".img_list1 li, .img_list2 li").css("width", window_width);
 
 
-		/*********************   Imgscroll  *********************/
-		function imgScroll(wraper, speed){
-			var wraper = $(wraper);
-			var img_list = wraper.find('ul');
-			var w = img_list.find('li').outerWidth(true);
-			img_list.animate({
-				'margin-left': -w
-				}, speed,
-				function() {
-					img_list.find('li').eq(0).appendTo(img_list);
-					img_list.css({'margin-left': 0});
-			});
-
-		}
-
-
 		/*********************   shining_stars  *********************/
-		function shiningStars(star) {
-			var position_x = randomNum(0, 1200, "float");
-			var position_y = randomNum(0, 900, "float");
+		function twinkleStars(star) {
+			var coordinate_x = randomNum(0, 1200, "float");
+			var coordinate_y = randomNum(0, 900, "float");
 			$(".star").css({
 				"position": "absolute",
-				"top": position_x,
-				"left": position_y
+				"top": coordinate_x,
+				"left": coordinate_y
 				}).hide().fadeIn(1000).delay(1000).fadeOut(1000);
 		};
 
-	    function randomNum(start, stop, type) {
-	        var rand_num = makeRandom(stop);
-	        while(rand_num < start){
-	            rand_num = makeRandom(stop);
-	        }
-	        if(type == "int") {
-	           rand_num = parseInt(rand_num);
-	        }
-	        console.log(rand_num);
-	        return rand_num;
-	    }
-
-	    function makeRandom(max) {
-	        return Math.random()*max;
-	    }
-
-		/*********************   cloud  *********************/
+		/*********************   floatCloud  *********************/
 		function floatCloud (cloud, direction, position, distance, speed) {
 			var cloud = $(cloud);
 
@@ -81,29 +49,29 @@
 		}
 
 		/*********************   conduct  *********************/
-		var x=setInterval(
-			function() {
-				imgScroll('.scrolling_block1', 400);
-				imgScroll('.scrolling_block2', 1000);
-				imgScroll('.scrolling_block3', 600);
-			}, 4000
-		);
+        var x=setInterval(
+            function() {
+                $(".scrolling_block1").imgScroll({direction: "to_right", speed: 400});
+                $(".scrolling_block2").imgScroll({direction: "to_right", speed: 1000});
+                $(".scrolling_block3").imgScroll({direction: "to_right", speed: 600});
+            }, 4000
+        );
 
-		$(".scrolling_block3").hover(function(){
-			clearInterval(x);
-		},function(){
-			x=setInterval(
-				function() {
-					imgScroll('.scrolling_block1', 400);
-					imgScroll('.scrolling_block2', 1000);
-					imgScroll('.scrolling_block3', 600);
-				}, 4000
-			);
-		});
+        $(".scrolling_block3").hover(function(){
+            clearInterval(x);
+        },function(){
+            x=setInterval(
+                function() {
+                $(".scrolling_block1").imgScroll({direction: "to_right", speed: 400});
+                $(".scrolling_block2").imgScroll({direction: "to_right", speed: 1000});
+                $(".scrolling_block3").imgScroll({direction: "to_right", speed: 600});
+                }, 4000
+            );
+        });
 
 /*		setInterval(
 			function() {
-				shiningStars(".star");
+				twinkleStars(".star");
 			}, 4000
 		);*/
 
